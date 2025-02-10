ea53612bbec7edb2ad5886a07445c3930d9d2bd1
Jesus Diaz <jesus740@gmail.com>
2025-02-10 16:59:01 +0100

    feat: wip - openings board part 2

--- components/TopBar/TopBar.tsx
+++ components/TopBar/TopBar.tsx
@@ -1,14 +1,15 @@
 import { FC } from "react";
 import { UserMenu } from "../UserMenu/UserMenu";
 import { Locale } from "@/i18n-config";
+import Image from "next/image";
 
 interface TopBarProps {
   lang: Locale;
 }
 export const TopBar: FC<TopBarProps> = ({ lang }) => {
   return (
-    <nav className="flex h-32 w-full items-center justify-between bg-gray-200 px-28">
-      <h1 className="text-2xl font-bold"> Logo </h1>
+    <nav className="relative flex h-20 w-full items-center justify-between bg-gray-200 px-8">
+      <Image width={200} height={40} src="/assets/logo.png" alt="TechCo" />
       <UserMenu lang={lang} username="Jesus D" />
     </nav>
   );
